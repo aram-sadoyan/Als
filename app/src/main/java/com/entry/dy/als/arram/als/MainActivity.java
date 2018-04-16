4c9435e59023fa23373f507fed4a7618f531ba7f
aramksadoyan <aram.sadoyan@picsart.com>
2018-04-16 11:02:54 +0400

    Initial commit

---
+++ app/src/main/java/com/entry/dy/als/arram/als/MainActivity.java
@@ -0,0 +1,17 @@
+package com.entry.dy.als.arram.als;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+
+
+	}
+}
